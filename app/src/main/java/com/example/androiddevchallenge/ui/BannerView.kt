5abc1bfccef66c78c2c08746beefbb00aee52762
wangpeng.rocky <wangpeng.rocky@bytedance.com>
2021-02-27 08:41:39 +0800

    fix klint check err

--- app/src/main/java/com/example/androiddevchallenge/ui/BannerView.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/BannerView.kt
@@ -3,7 +3,14 @@ package com.example.androiddevchallenge.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Card
